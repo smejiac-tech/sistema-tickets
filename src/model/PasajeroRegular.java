9d6a762728102c0e76dd27ffdbf1a14663cfa394
camilonav <candresnavarro@unicesar.edu.co>
2026-03-21 13:26:04 -0500

    refactor(model): agregar getTipo e imprimirDetalle a PasajeroRegular

--- src/model/PasajeroRegular.java
+++ src/model/PasajeroRegular.java
@@ -1,18 +1,26 @@
 package model;
-
+ 
 public class PasajeroRegular extends Pasajero {
-
+ 
     public PasajeroRegular(String cedula, String nombre) {
         super(cedula, nombre);
     }
-
+ 
     @Override
     public double calcularDescuento() {
-        return 0.0;
+        return 0.0; // Sin descuento
     }
-
+ 
+    @Override
+    public String getTipo() {
+        return "Regular";
+    }
+ 
     @Override
     public void imprimirDetalle() {
-        System.out.println("Pasajero Regular: " + nombre);
+        System.out.println("=== PASAJERO ===");
+        System.out.println("Tipo   : Regular (sin descuento)");
+        System.out.println("Cedula : " + cedula);
+        System.out.println("Nombre : " + nombre);
     }
 }
