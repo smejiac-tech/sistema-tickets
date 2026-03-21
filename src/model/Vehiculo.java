7cbd17f560192a9773fee9ba4e100633772feeb4
Ronal Samith Palacio Teran <rspalacio@unicesar.edu.co>
2026-03-21 14:07:53 -0500

    fix: actualización de vehiculo en model

--- src/model/Vehiculo.java
+++ src/model/Vehiculo.java
@@ -4,76 +4,47 @@
  */
 package model;
 
-/**
- *
- * @author ronal
- */
-public abstract class Vehiculo {
-    
+public abstract class Vehiculo implements Imprimible {
+
     protected String placa;
-    protected Ruta ruta;
+    protected String ruta;
     protected int capacidadMaxima;
     protected int pasajerosActuales;
-    protected double tarifaBase;
 
-    public Vehiculo(String placa, Ruta ruta, int capacidadMaxima, double tarifaBase) {
+    public Vehiculo(String placa, String ruta, int capacidadMaxima) {
         this.placa = placa;
         this.ruta = ruta;
         this.capacidadMaxima = capacidadMaxima;
-        this.tarifaBase = tarifaBase;
         this.pasajerosActuales = 0;
     }
 
     public boolean hayCupos() {
         return pasajerosActuales < capacidadMaxima;
     }
-    public String getPlaca() {
-    return placa;
-}
 
-public void setPlaca(String placa) {
-    this.placa = placa;
-}
+    public void subirPasajero() {
+        if (hayCupos()) {
+            pasajerosActuales++;
+        }
+    }
 
-public Ruta getRuta() {
-    return ruta;
-}
+    public int getCapacidadDisponible() {
+        return capacidadMaxima - pasajerosActuales;
+    }
 
-public void setRuta(Ruta ruta) {
-    this.ruta = ruta;
-}
+    public String getPlaca() { return placa; }
+    public String getRuta()  { return ruta;  }
 
-public int getCapacidadMaxima() {
-    return capacidadMaxima;
-}
+    // Cada subclase devuelve su tarifa base
+    public abstract double getTarifa();
 
-public void setCapacidadMaxima(int capacidadMaxima) {
-    this.capacidadMaxima = capacidadMaxima;
-}
-
-public int getPasajerosActuales() {
-    return pasajerosActuales;
-}
-
-public void setPasajerosActuales(int pasajerosActuales) {
-    this.pasajerosActuales = pasajerosActuales;
-}
-
-public double getTarifaBase() {
-    return tarifaBase;
-}
-
-public void setTarifaBase(double tarifaBase) {
-    this.tarifaBase = tarifaBase;
-}
-@Override
-public String toString() {
-    return "Vehiculo{" +
-            "placa='" + placa + '\'' +
-            ", ruta=" + ruta +
-            ", capacidadMaxima=" + capacidadMaxima +
-            ", pasajerosActuales=" + pasajerosActuales +
-            ", tarifaBase=" + tarifaBase +
-            '}';
-}
+    @Override
+    public void imprimirDetalle() {
+        System.out.println("Vehiculo : " + getClass().getSimpleName());
+        System.out.println("Placa    : " + placa);
+        System.out.println("Ruta     : " + ruta);
+        System.out.println("Capacidad: " + capacidadMaxima);
+        System.out.println("Cupos    : " + getCapacidadDisponible());
+        System.out.printf ("Tarifa   : $%.0f%n", getTarifa());
+    }
 }
